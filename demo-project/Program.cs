97945efe7089930600916d4fa6266fed2082a72e
Josh Tucholski <josh@techelevator.com>
2017-09-13 15:57:45 -0400

    Added generation for #9

--- demo-project/Program.cs
+++ demo-project/Program.cs
@@ -10,6 +10,50 @@ namespace demo_project
     {
         static void Main(string[] args)
         {
+            BlockNumberGenerator generator = new BlockNumberGenerator();
+
+            Console.WriteLine(generator.GetNumber(9));
+
+
+
+        //    Console.WriteLine("Welcome!");
+        //    string[] options = { "rock", "paper", "scissors" };
+
+        //    Console.WriteLine("Make a guess (1 for rock, 2 for paper, 3 for scissors)");
+        //    int choice = int.Parse(Console.ReadLine());
+        //    string weapon = options[choice - 1];
+
+        //    Random rnd = new Random();
+        //    int computerChoice = rnd.Next(3);
+        //    string computerWeapon = options[computerChoice];
+
+        //    Console.WriteLine();
+        //    Console.WriteLine(3);
+        //    System.Threading.Thread.Sleep(5000); // 5,000 ms
+
+
+        //    Console.WriteLine(2);
+        //    System.Threading.Thread.Sleep(5000); // 5,000 ms
+
+        //    Console.WriteLine(1);
+        //    System.Threading.Thread.Sleep(5000); // 5,000 ms
+
+        //    Console.WriteLine($"You picked {weapon}");
+        //    Console.WriteLine($"Computer picked {computerWeapon}");
+
+        //    if (weapon == "paper" && computerWeapon == "rock")
+        //    {
+        //        Console.WriteLine("You win! Paper Covers Rock");
+        //    }
+        //    else if (weapon == "scissors" && computerWeapon == "paper")
+        //    {
+        //        Console.WriteLine("You win! Scissors cuts paper");
+        //    }
+            
+
+
+
+
         }
     }
 }
